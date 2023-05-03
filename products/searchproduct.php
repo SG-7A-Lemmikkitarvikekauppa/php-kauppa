c500c2c0e601119970ca4511384b994456c3b1c6
sofiakurkela <sofiakurkelaa@gmail.com>
2023-05-03 11:46:07 +0300

    tuote

--- products/searchproduct.php
+++ products/searchproduct.php
@@ -11,7 +11,7 @@ $phrase = $parameters[1];
 
 try{
     $db = openDb();
-    $sql = "select * from asiakas where name like '%phrase%'";
+    $sql = "select * from tuote where name like '%phrase%'";
     selectAsJson($db,$sql);
 }
 
